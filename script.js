12080ff0f2486358ba15a3a07f8e61cc8c3eaf15
XMKT <bezrukov005@gmail.com>
2025-12-23 05:07:14 +0300

    44. Выносим JavaScript в отдельный файл

---
+++ script.js
@@ -0,0 +1,8 @@
+const likeButton = document.getElementById("likeBtn");
+const likeCount = document.getElementById("likeCount");
+
+let count = 0;
+likeButton.addEventListener("click", function () {
+  count++;
+  likeCount.textContent = "Likes: " + count;
+});
